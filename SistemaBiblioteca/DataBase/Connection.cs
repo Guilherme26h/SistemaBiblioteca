4f627bbb4ad536da6371ed8a4140ecdabfd788da
Guilherme Henrique de Sousa Jesus <guilherme26h@gmail.com>
2022-01-26 15:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaBiblioteca/DataBase/Connection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Classe de Conexão com banco de dados
+ */
+namespace SistemaBiblioteca
+{
+    public class Connection
+    {
+        SqlConnection con = new SqlConnection();
+        private string DB = @"Data Source=DEV-GUILHERME\SQLEXPRESS01;Initial Catalog = Biblioteca; Integrated Security = True";
+
+        public Connection()
+        {
+            con.ConnectionString = DB;
+        }        
+
+        public SqlConnection conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
